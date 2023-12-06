f08be90bafe29f06bd68fd5b6d6cc28386ac11ec
nongj <nongj@hellmaster>
2023-12-05 19:59:46 -0500

    updated java code

--- PracticeGitWithJAVA/src/git/practice/GitPractice.java
+++
@@ -1,8 +0,0 @@
-package git.practice;
-
-public class GitPractice {
-	public static void main(String[] args) {
-		System.out.println("This is first git project I am learning");
-	}
-
-}
---
+++ PracticeGitWithJAVA/src/gitpractice/GitPractice.java
@@ -0,0 +1,10 @@
+package gitpractice;
+
+public class GitPractice {
+	
+	public static void main(String[] args) {
+		System.out.println(" This is my  first git project I am learning ");
+		System.out.println("I won't give up");
+	}
+
+}
