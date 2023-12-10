bf375f038f431e70d5fd5ac4afdac3765041790a
nongj <nongj@hellmaster>
2023-12-10 15:10:52 -0500

    I change my code to see the different between the code

--- PracticeGitWithJAVA/src/gitpractice/GitPractice.java
+++ PracticeGitWithJAVA/src/gitpractice/GitPractice.java
@@ -5,6 +5,18 @@ public class GitPractice {
 	public static void main(String[] args) {
 		System.out.println(" This is my  first git project I am learning ");
 		System.out.println("I won't give up");
+		int n = 5;
+		for(int i = 1; i <= n; i++) {
+			for(int j = 1; j <= n; j++) {
+				System.out.print("  ");
+				
+			}
+			for(int j = i; j <= n; j++) {
+				System.out.print("* ");
+			}
+			
+		System.out.println("");	
+		}
 	}
 
 }
---
+++ PracticeGitWithJAVA/src/gitpractice/NewTest.java
@@ -0,0 +1,8 @@
+package gitpractice;
+
+public class NewTest {
+	
+		public static void main(String[] args) {
+			System.out.println("I love java");
+		}
+}
